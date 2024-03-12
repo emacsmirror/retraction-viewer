cb2dbc933b42cd7ef3119592137855c50ed968a4
Samuel W. Flint <swflint@flintfam.org>
2024-03-12 18:56:24 -0500

    Add stub package file

---
+++ retraction-viewer.el
@@ -0,0 +1,35 @@
+;;; retraction-viewer.el --- View retraction information for current citation  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Samuel W. Flint
+
+;; Author: Samuel W. Flint <me@samuelwflint.com>
+;; Version: 0.0.1
+;; Keywords: bib, tex, data
+;; URL: https://git.sr.ht/~swflint/retraction-viewer
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; SPDX-FileCopyrightText: 2024 Samuel W. Flint <swflint@flintfam.org>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; TODO
+
+;;; Code:
+
+
+
+(provide 'retraction-viewer)
+
+;;; retraction-viewer.el ends here
